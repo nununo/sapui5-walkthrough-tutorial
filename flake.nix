39a8c22494fae19eab4e8d08e10fd34b5cc40fd4
Nuno Godinho <mail@nunogodinho.com>
2024-07-30 16:06:42 +0100

    Flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Abapinho.com with Hugo and netlify-cli";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.nodePackages.nodejs
+          ];
+          buildInputs = [
+          ];
+        };
+      });
+}
